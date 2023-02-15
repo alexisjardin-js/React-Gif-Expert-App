4a14b9d17aafe7bcae3e1a0248f7fcb71a15d686
alexis jardin <alexis.david.jardin@gmail.com>
2023-02-15 11:41:42 -0300

    agregando test a componentes

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -1,5 +1,6 @@
 
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
 export const AddCategory = ({onNewCategory}) => {
 
@@ -19,7 +20,7 @@ export const AddCategory = ({onNewCategory}) => {
   }
 
   return (
-    <form onSubmit={onSubmit}>
+    <form onSubmit={onSubmit} aria-label="form">
       <input
         type='text'
         placeholder='Buscar Gifs'
@@ -31,6 +32,11 @@ export const AddCategory = ({onNewCategory}) => {
 
 
   )
+}
+
+AddCategory.propTypes = {
+  onNewCategory: PropTypes.func.isRequired,
+
 }
 
 
